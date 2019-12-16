9f5d65f6e9be80f004f9666c529add7e538127dc
Amit Singh <star.amitks@gmail.com>
2019-12-16 15:09:49 +0530

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MarketComponent } from './market/market.component';
+import { RecommendationComponent } from './recommendation/recommendation.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:'', component: HomeComponent},
+  { path:'home', component: HomeComponent},
+  { path:'recommendation', component: RecommendationComponent},
+  { path:'market', component: MarketComponent},
+  { path:'subscribe', component: SubscribeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
